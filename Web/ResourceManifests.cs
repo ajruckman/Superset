6d73704ba0fd48dd12bde0169cac42e18f459469
A.J. Ruckman <aj@ruckman.dev>
2020-03-05 18:54:48 -0500

    - Move ResourceManifests to /Web/

--- Web/ResourceManifests.cs
+++ Web/ResourceManifests.cs
@@ -1,11 +1,12 @@
 ﻿using Superset.Web.Resources;
 
-namespace Superset.Web.ResourceManifests
+// ReSharper disable once CheckNamespace
+namespace Superset.Web
 {
     public static class ResourceManifests
     {
         public static ResourceManifest SaveAsFile =
-            new ResourceManifest(nameof(Superset), scripts: new[] {"js/SaveAsFile.js"});
+            new ResourceManifest(nameof(Superset), new[] {"js/SaveAsFile.js"});
 
         public static ResourceManifest LocalCSS =
             new ResourceManifest(nameof(Superset), stylesheetsExternal: new[] {"/css/Local.css"});
