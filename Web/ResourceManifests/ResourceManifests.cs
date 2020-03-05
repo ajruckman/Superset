8bc0c6d741d5e364b5770f47cc767b6aa17fd377
A.J. Ruckman <aj@ruckman.dev>
2020-03-05 18:52:49 -0500

    - Add LocalCSS manifest for /css/Local.css

--- Web/ResourceManifests/ResourceManifests.cs
+++ Web/ResourceManifests/ResourceManifests.cs
@@ -6,5 +6,8 @@ namespace Superset.Web.ResourceManifests
     {
         public static ResourceManifest SaveAsFile =
             new ResourceManifest(nameof(Superset), scripts: new[] {"js/SaveAsFile.js"});
+
+        public static ResourceManifest LocalCSS =
+            new ResourceManifest(nameof(Superset), stylesheetsExternal: new[] {"/css/Local.css"});
     }
 }
