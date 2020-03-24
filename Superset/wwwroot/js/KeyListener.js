16e98f7d08672c14da3174c42583abe82ada9ea9
AJ Ruckman <aj.ruckman@mckee.com>
2020-03-24 17:36:52 -0400

    remove logging

--- Superset/wwwroot/js/KeyListener.js
+++ Superset/wwwroot/js/KeyListener.js
@@ -1,5 +1,4 @@
 window.Superset_AddKeyCallback = function (target, dotnetHelper) {
-    console.log(target);
     window.Superset_KeyListeners[Math.random().toString(36).substring(2, 15)] = {
         target: target,
         dotnetHelper: dotnetHelper,
@@ -21,10 +20,6 @@ if (window.Superset_KeyListeners == null) {
             }
 
             let listener = window.Superset_KeyListeners[key];
-
-            console.log(e);
-            console.log(key);
-            console.log(key.target);
 
             if (listener.target.isEqualNode(e.target)) {
                 listener.dotnetHelper.invokeMethodAsync("KeyUp", e.key, e.target.id, e.shiftKey, e.ctrlKey);
