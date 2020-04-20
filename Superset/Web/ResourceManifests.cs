0fe3cffcafa8ebc8d81687558fa5e70b73a31f7b
AJ Ruckman <aj.ruckman@mckee.com>
2020-04-20 10:43:50 -0400

    Superset 1.15.5-alpha: Fix Tooltip script and resource manifest

--- Superset/Web/ResourceManifests.cs
+++ Superset/Web/ResourceManifests.cs
@@ -12,7 +12,8 @@ namespace Superset.Web
             new ResourceManifest(nameof(Superset), new[] {"js/FocusElement.js"});
 
         public static ResourceManifest Listeners =
-            new ResourceManifest(nameof(Superset), new[] {"js/Utilities.js", "js/ClickListener.js", "js/KeyListener.js"});
+            new ResourceManifest(nameof(Superset),
+                new[] {"js/Utilities.js", "js/ClickListener.js", "js/KeyListener.js"});
 
         public static ResourceManifest LocalCSS =
             new ResourceManifest(nameof(Superset), stylesheetsExternal: new[] {"/css/Local.css"});
@@ -20,5 +21,14 @@ namespace Superset.Web
         public static ResourceManifest LocalStyledCSS =
             new ResourceManifest(nameof(Superset),
                 stylesheetsExternal: new[] {"/css/Local_Style.{{ThemeVariant}}.css"});
+
+        public static ResourceManifest Tooltip =
+            new ResourceManifest(nameof(Superset),
+                scriptsExternal: new[]
+                {
+                    "https://unpkg.com/@popperjs/core@2.3.3/dist/umd/popper.min.js", // https://unpkg.com/@popperjs/core@2
+                    "https://unpkg.com/tippy.js@6.2.0/dist/tippy-bundle.umd.min.js" // https://unpkg.com/tippy.js@6
+                },
+                scripts: new []{"js/Tooltip.js"});
     }
 }
---
+++ Superset/wwwroot/js/Tooltip.js
@@ -0,0 +1,8 @@
+window.Superset_InitTooltip = function (target, text) {
+    console.log(target);
+    tippy(target, {
+        content: text,
+        distance: 3,
+        theme: 'rudiment',
+    });
+};
