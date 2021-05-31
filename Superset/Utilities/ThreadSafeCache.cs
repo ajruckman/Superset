ca687c3ab98d16d26fcc06ad3f8954589121a8dc
AJ Ruckman <40143425+ajruckman@users.noreply.github.com>
2021-05-31 09:05:02 -0400

    Super set 1.18.0-alpha - ThreadSafeCache nullability changes

--- Superset/Utilities/ThreadSafeCache.cs
+++ Superset/Utilities/ThreadSafeCache.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Superset.Utilities
 {
-    public class ThreadSafeCache<T> where T : class?
+    public class ThreadSafeCache<T> where T : class
     {
-        private readonly object _lock  = new object();
-        private          T      _value = default!;
-        private          bool   _hasSet;
+        public delegate T ValueGetter();
+
+        private readonly object _lock = new();
+
+        private T?   _value;
+        private bool _hasSet;
 
         public bool HasValue
         {
@@ -14,7 +18,7 @@ namespace Superset.Utilities
             {
                 lock (_lock)
                 {
-                    return _value != null || _hasSet;
+                    return _hasSet;
                 }
             }
         }
@@ -23,38 +27,51 @@ namespace Superset.Utilities
         {
             lock (_lock)
             {
-                if (_value == null)
+                if (!_hasSet)
                     throw new InvalidOperationException("Call to ThreadSafeCache.Get() with unset value.");
-                return _value;
+                return _value!;
             }
         }
 
-        public T Set(T newValue, bool final = false)
+        public bool TryGet([NotNullWhen(returnValue: true)] out T? result)
         {
             lock (_lock)
             {
-                if (final) _hasSet = true;
+                if (_hasSet)
+                {
+                    result = _value!;
+                    return true;
+                }
+
+                result = null;
+                return false;
+            }
+        }
+
+        public T Set(T newValue)
+        {
+            lock (_lock)
+            {
+                _hasSet = true;
                 return _value = newValue;
             }
         }
 
-        public delegate T ValueGetter();
-
-        public T SetIf(ValueGetter valueGetter, bool final = false)
+        public T SetIf(ValueGetter valueGetter)
         {
             lock (_lock)
             {
-                if (final) _hasSet = true;
+                _hasSet = true;
                 return _value ??= valueGetter.Invoke();
             }
         }
 
-        public void Invalidate()
+        public void Clear()
         {
             lock (_lock)
             {
                 _hasSet = false;
-                _value  = default!;
+                _value  = null;
             }
         }
     }
